bc3b73e8f6c598bfb4d371e912d4dae9b077018e
FabioCividini <fabiocividini22@gmail.com>
2021-11-25 17:23:11 +0100

    Esercizio finito

--- src/it/shop/Main.java
+++ src/it/shop/Main.java
@@ -4,8 +4,18 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		System.out.println("NUOVO PRODOTTO 1 DELLO SHOP");
 		
-
+		Prodotto prodottoScarpe = new Prodotto("Scarpe", "Da uomo taglia 44", 10);
+		
+		prodottoScarpe.infoProdotto();
+		
+		
+//		System.out.println("\nNUOVO PRODOTTO 2 DELLO SHOP");
+//		
+//		Prodotto prodottoMaglietta = new Prodotto("Maglietta", "Da uomo taglia XXL", 40);
+//		
+//		prodottoMaglietta.infoProdotto();
 	}
 
 }
--- src/it/shop/Prodotto.java
+++ src/it/shop/Prodotto.java
@@ -1,5 +1,107 @@
 package it.shop;
 
-public class Prodotto {
+import java.util.Random;
 
+public class Prodotto {
+	
+	
+	//attributi
+	private int codice;
+	private String nome;
+	private String descrizione;
+	private float prezzo;
+	private float iva = (float) 0.22;
+	
+	
+	//costruttore del prodotto
+	public Prodotto(String nome, String descrizione, float prezzo) {
+		this.codice = generaCodice();
+		this.nome = nome;
+		this.prezzo = prezzo;
+		this.descrizione = descrizione;
+	}
+	
+	
+	public String getNome() {
+		return nome;
+	}
+
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+
+
+	public float getPrezzo() {
+		return prezzo;
+	}
+
+
+
+	public void setPrezzo(float prezzo) {
+		this.prezzo = prezzo;
+	}
+
+
+
+	public float getIva() {
+		return iva;
+	}
+
+
+
+	public void setIva(float iva) {
+		this.iva = iva;
+	}
+
+
+
+	public int getCodice() {
+		return codice;
+	}
+
+
+
+	//metodo per valorizzare il codice con un metodo random
+	public int generaCodice() {
+		Random random = new Random();
+		return random.nextInt(100);
+	}
+	
+	//metodo genera prezzo con IVA
+	public float generaPrezzoIva() {
+		float generaPrezzoIva = prezzo + (prezzo * iva);
+		return generaPrezzoIva;
+	}
+	
+	//metodo genera nome esteso del prodotto
+	public String nomeEsteso() {
+		String nomeEsteso = getCodice() + " - " + nome;
+		return nomeEsteso;
+	}
+	
+	//metodo per stampare tutte le info del prodotto
+	void infoProdotto() {
+		System.out.println("Codice ID prodotto: " + getCodice());
+		System.out.println("Nome prodotto: " + getNome());
+		System.out.println("Nome esteso prodotto: " + nomeEsteso());
+		System.out.println("Descrizione prodotto: " + getDescrizione());
+		System.out.println("Prezzo base prodotto: " + getPrezzo() + "€");
+		System.out.println("Prezzo prodotto con IVA: " + generaPrezzoIva() + "€");
+	}
+	
 }
